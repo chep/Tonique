662d055fe5f56babc9678c0e65c3a5c3f06c41ac
Cédric Chépied <cedric.chepied@gmail.com>
2021-09-19 21:47:02 +0200

    This branch is so dirty, even your mom can't clean it.

--- mainwindow.py
+++ mainwindow.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2021, Cédric Chépied. All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2.0, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License, version 2.0, for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+
 import random
 from threading import Timer, Thread
 from PyQt5.QtWidgets import QMainWindow, QMessageBox, QWidget
@@ -20,7 +35,7 @@ CORDES = ["Mi", "Si", "Sol", "Ré", "La", "Mi"]
 POSITIONS = {6: [(3, -3), (4, +2)],
              5: [(3, +2), (2, -2)],
              4: [(2, +3), (1, -2)],
-             3: [(1, +4)]}
+             3: [(1, +3)]}
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
--- widget.py
+++ widget.py
@@ -1,4 +1,20 @@
-from PyQt5.QtWidgets import QWidget
+# Copyright (c) 2021, Cédric Chépied. All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 2.0, as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License, version 2.0, for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+
+
+ from PyQt5.QtWidgets import QWidget
 from PyQt5.QtGui import QPainter, QPixmap, QPen, QBrush, QColor
 from PyQt5.QtCore import QPoint, QLine
 from PyQt5.Qt import Qt, pyqtSignal
